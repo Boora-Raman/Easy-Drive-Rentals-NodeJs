55f9ff0115822a66237e8f26320d47e1940470ac
Boora-Raman <157504430+Boora-Raman@users.noreply.github.com>
2025-04-01 20:58:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-mongoose.connect('mongodb+srv://booraraman2004:ramanBoora@cluster0.xaz6fpx.mongodb.net/RentalData', {
+mongoose.connect('//yourDataBase_URI', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
